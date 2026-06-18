e2906fec7ba9652330f462b9368d9efcc01a26d4
vishal shivnani <156511076+VishalPython1594@users.noreply.github.com>
2026-06-18 17:53:22 +0530

    update asset

--- challenge/chaincode/asset_contract.go
+++ challenge/chaincode/asset_contract.go
@@ -1,8 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+type SmartContract struct {
+	contractapi.Contract
+}
+
+type Asset struct {
+	ID    string `json:"ID"`
+	Name  string `json:"Name"`
+	Owner string `json:"Owner"`
+}
+
+// CreateAsset creates a new asset
+func (s *SmartContract) CreateAsset(
+	ctx contractapi.TransactionContextInterface,
+	id string,
+	name string,
+	owner string,
+) error {
+
+	asset := Asset{
+		ID:    id,
+		Name:  name,
+		Owner: owner,
+	}
+
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(id, assetJSON)
+}
+
+// ReadAsset reads an asset
+func (s *SmartContract) ReadAsset(
+	ctx contractapi.TransactionContextInterface,
+	id string,
+) (*Asset, error) {
+
+	assetJSON, err := ctx.GetStub().GetState(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if assetJSON == nil {
+		return nil, fmt.Errorf("asset %s does not exist", id)
+	}
+
+	var asset Asset
+
+	err = json.Unmarshal(assetJSON, &asset)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &asset, nil
+}
+
+// =========================
+// Candidate Task
+// =========================
 func (s *SmartContract) AssetExists(
-    ctx contractapi.TransactionContextInterface,
-    id string,
+	ctx contractapi.TransactionContextInterface,
+	id string,
 ) (bool, error) {
 
-    // TODO
+	// TODO: Implement this function
 
+	return false, nil
 }
